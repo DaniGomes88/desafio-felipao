6d8f110994b84dd59450e118d52c10184b2e49b2
DaniGomes88 <gomes.danieldossantos@gmail.com>
2024-01-04 15:07:53 -0300

    Desafio terminado

---
+++ src/index_prompt.js
@@ -0,0 +1,10 @@
+var prompt = require('prompt-sync')();
+
+console.log("Olá, seja bem vindo(a) ao DESAFIO CLASSIFICADOR DE NÍVEL DE HERÓI!!!")
+let nomeHeroi = prompt("Digite o nome do seu Herói: ");
+let xpHeroi = prompt("Digite a quantidade de XP do seu Herói: ");
+let nivelHeroi = ""
+
+
+
+
